a438f4b15afb7d4ca2baa4fa93cc19805f5dfdb1
lihasahil <sahilshrestha106@gmail.com>
2025-05-04 12:55:09 +0545

    Completed Prject

---
+++ src/components/FollowButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import toast from "react-hot-toast";
+import { toggleFollow } from "@/actions/user.action";
+
+function FollowButton({ userId }: { userId: string }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFollow = async () => {
+    setIsLoading(true);
+    try {
+      await toggleFollow(userId);
+      toast.success("User followed Successfully!!");
+    } catch (error) {
+      toast.error("Error following User");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Button
+      size={"sm"}
+      variant={"secondary"}
+      onClick={handleFollow}
+      disabled={isLoading}
+      className="w-20"
+    ></Button>
+  );
+}
+
+export default FollowButton;
